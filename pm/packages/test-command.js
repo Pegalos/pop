2ecd4d55a400af7373887d9a616b4dfac414feb7
Blocks <steve.smith@frenchn1270.site>
2025-03-12 13:15:41 -0500

    Update test-command.js

--- pm/packages/test-command.js
+++ pm/packages/test-command.js
@@ -1,8 +1,3 @@
-// Register the "test" command
-async function runCommand(command) {
-  if (command === "test") {
-    return "Hello, World!"; // Output for the "test" command
-  } else {
-    return `Command "${command}" not recognized.`;
-  }
+function test() {
+  console.log("Hello, World!");
 }
