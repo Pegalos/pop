936b047a28138eab981808e086cbfd4188ee31e1
Blocks <steve.smith@frenchn1270.site>
2025-03-12 13:59:11 -0500

    Update test-command.js

--- pm/packages/test-command.js
+++ pm/packages/test-command.js
@@ -1,4 +1,4 @@
-// test-command.js
-function testCommand(args) {
-  return "Hello World!";
-}
+// This is the custom command for "test"
+// When executed, it will return "Hello World!"
+
+return "Hello World!";
