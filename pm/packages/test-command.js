bf57faeeab624f2d7d26e698933799d271b8940c
Blocks <steve.smith@frenchn1270.site>
2025-03-12 13:34:22 -0500

    Update test-command.js

--- pm/packages/test-command.js
+++ pm/packages/test-command.js
@@ -1,4 +1,18 @@
-function testCommand() {
-  console.log("Hello, world!");
+async function handleCommand(cmd, args) {
+  let output = "";
+
+  switch (cmd) {
+    case "test":
+      output = "hello, world!";
+      break;
+      
+    default:
+      if (commandRegistry[cmd]) {
+        output = commandRegistry[cmd](args); // Run installed command
+      } else {
+        output = `Command "${cmd}" not found.`;
+      }
+  }
+
+  return output;
 }
-testCommand();
