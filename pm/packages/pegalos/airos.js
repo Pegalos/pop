189372f2ba2f75a366b4090bf0f9e36eb4b008c2
Blocks <steve.smith@frenchn1270.site>
2025-09-05 14:48:23 -0500

    Update airos.js

--- pm/packages/pegalos/airos.js
+++ pm/packages/pegalos/airos.js
@@ -1,4 +1,14 @@
-const cmd = args && args[0];
-if (cmd === "test") {
-    return "hello world";
+function cmds(args) {
+    const timestamp = new Date().toISOString().replace('T', ' ').split('.')[0];
+    const cmdArgs = args.slice(1);
+    if (!cmdArgs || cmdArgs.length === 0) {
+        return;
+    }
+    if (cmdArgs[0] === 'test') {
+        return `Hello, world! [${timestamp}]`;
+    } else {
+        return `bm:usage [${timestamp}] Usage: bm hello`;
+    }
 }
+
+return cmds(args);
