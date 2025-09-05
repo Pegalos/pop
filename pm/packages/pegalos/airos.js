516d49aa099428420eb9d5d7232130609e842acb
Blocks <steve.smith@frenchn1270.site>
2025-09-05 14:45:55 -0500

    Update airos.js

--- pm/packages/pegalos/airos.js
+++ pm/packages/pegalos/airos.js
@@ -1,7 +1,4 @@
-async function commands(args) {
-    const cmd = args && args[0];
-    if (cmd === "test") {
-        return "hello world";
-    }
-    // Add more commands as needed
+const cmd = args && args[0];
+if (cmd === "test") {
+    return "hello world";
 }
